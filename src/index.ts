d2fec6613f1402f0128560f80f2315f49009808e
Xavier Zhou <xiayezhou@gmail.com>
2023-01-03 16:32:55 +0800

    feat: 1st impl

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { Redis } from 'ioredis';
+import { v4 as uuid } from 'uuid';
 /**
  * A simple lock with redis implementing the pattern: https://redis.io/commands/set/
  * It does not aim to have a fault tolerant solution like redlock as we do not have such use case.
  * */
 
-export async function acquire(redis: Redis, resource: string, ttl: number) {}
+export async function acquire(redis: Redis, resource: string, ttl: number) {
+  const token = uuid();
+  const lockKey = `lock:${resource}`;
+  const ack = await redis.set(lockKey, token, 'EX', ttl, 'NX');
+  if (ack !== 'OK') {
+    return false;
+  }
+  return true;
+}
 
 async function release(redis: Redis, token: string, lockKey: string) {}
