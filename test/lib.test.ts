228eedb491a94c39c0ae000f3aee2bb08d9b8c74
Xavier Zhou <xiayezhou@gmail.com>
2023-01-03 16:03:44 +0800

    docs: acquire interface

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -2,5 +2,7 @@ import * as srl from '../src/index';
 // const { acquire } = srl;
 
 describe('SimpleRedisLock', () => {
-  it.todo('should be able to lock');
+  it.todo('should be able to acquire the lock');
+  // input: redis, the identifier of locked resource, ttl for auto-release
+  // output: lock result
 });
